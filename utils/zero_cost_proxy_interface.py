ea23b636bd25153f7c9e0318cb55037961f685a1
Zaim Imran <zaim.imran@gmail.com>
2023-01-27 14:09:44 +0100

    Update zero_cost_proxy_interface.py

--- utils/zero_cost_proxy_interface.py
+++ utils/zero_cost_proxy_interface.py
@@ -1,3 +1,3 @@
 class ZeroCostProxyInterface:
-    def calculate_proxy(self, net, data_loader, hyperparameters, device, loss_function, eval = False, train = True,  single_batch = True, bn = False) -> float:
+    def calculate_proxy(self, net, data_loader, device, loss_function, eval = False, train = True,  single_batch = True, bn = False) -> float:
         pass
