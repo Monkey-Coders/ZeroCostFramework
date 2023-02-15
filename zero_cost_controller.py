b81c0b96bd2ed10e991771b24309f420361bc98d
Zaim Imran <zaim.imran@gmail.com>
2023-02-15 13:29:02 +0100

    Update zero_cost_controller.py

--- zero_cost_controller.py
+++ zero_cost_controller.py
@@ -17,12 +17,12 @@ def import_class(name):
         mod = getattr(mod, comp)
     return mod
 
-def calculate_zc_proxy_scores(net, data_loader, hyperparameters, device, loss_function, save_path):
+def calculate_zc_proxy_scores(net, data_loader, device, loss_function, save_path):
     proxies = [f.replace(".py", "") for f in os.listdir(f"{os.path.dirname(__file__)}/zero_cost_proxies") if files_filter(f)]
     folder_name = os.path.dirname(__file__).split('/')[-1]
     for proxy_name in proxies:
         name = f"{folder_name}.{proxy_name}"
         proxy = import_class(name)
-        score, elapsed_time = calculate_function_runtime(proxy.calculate_proxy, net, data_loader, hyperparameters, device, loss_function)
+        score, elapsed_time = calculate_function_runtime(proxy.calculate_proxy, net, data_loader, device, loss_function)
         print('Execution time:', elapsed_time, 'seconds for proxy:', proxy_name)
         
