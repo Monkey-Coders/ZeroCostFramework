ebe248ff387b44a12d00d4a4ab10c0bfb26a9e8d
Zaim Imran <zaim.imran@gmail.com>
2023-03-01 12:42:21 +0100

    cleanup

--- zero_cost_controller.py
+++ zero_cost_controller.py
@@ -1,13 +1,6 @@
 import os
 from tqdm import tqdm
-from ZeroCostFramework.utils.util_functions import calculate_function_runtime
-
-def files_filter(f):
-    if f == '__init__.py':
-        return False
-    if f.endswith('.py'):
-        return True
-    return False
+from utils.util_functions import calculate_function_runtime, get_proxies
 
 def import_class(name):
     components = name.split('.')
@@ -17,7 +10,7 @@ def import_class(name):
     return mod
 
 def calculate_zc_proxy_scores(net, data_loader, device, loss_function, save_path):
-    proxies = [f.replace(".py", "") for f in os.listdir(f"{os.path.dirname(__file__)}/zero_cost_proxies") if files_filter(f)]
+    proxies = get_proxies()
     folder_name = os.path.dirname(__file__).split('/')[-1]
 
     scores = {}
@@ -27,8 +20,8 @@ def calculate_zc_proxy_scores(net, data_loader, device, loss_function, save_path
         proxy = import_class(name)
         score, elapsed_time = calculate_function_runtime(proxy().calculate_proxy, net, data_loader, device, loss_function)    
         scores[proxy_name] = {
-            "score": str(score),
-            "time": str(elapsed_time)
+            "score": score,
+            "time": elapsed_time
             }
         
     
